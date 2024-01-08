380c294c4f6de3a26287c4027a371cfc504bf3f5
dizzysch <victorwilbert@gmail.com>
2024-01-08 10:07:09 -0300

    tests: add CreateOrderCommandTests

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void DadoComandoInvalidoPedidoNaoDeveSerGerado()
+        {
+            var command = new CreateOrderCommand
+            {
+                Customer = "",
+                ZipCode = "18423568",
+                PromoCode = "12345678"
+            };
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+    }
+}
